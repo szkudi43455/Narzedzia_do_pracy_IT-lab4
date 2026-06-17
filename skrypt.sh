8f9a90454e210b725fdfbad9547ad83d02a7c0ba
Damian <dsw43455@student.dsw.edu.pl>
2026-06-17 04:15:04 +0200

    logs z argumentem liczby

--- skrypt.sh
+++ skrypt.sh
@@ -4,3 +4,12 @@
 if [ "$1" == "--date" ]; then
     date
 fi
+
+
+if [ "$1" == "--logs" ]; then
+    liczba=${2:-100}
+    for ((i=1;i<=liczba;i++))
+    do
+        echo "log$i.txt skrypt.sh $(date)" > log$i.txt
+    done
+fi
